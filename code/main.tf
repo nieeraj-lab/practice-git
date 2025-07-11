a3a199217f78b6a0a8a6b435b56f19547275a1aa
Neeraj Kumar <neeraj1.jnv@gmail.com>
2025-07-11 21:24:44 +0530

    Ek dusra RG bhi add kiye hai

--- code/main.tf
+++ code/main.tf
@@ -3,7 +3,7 @@ resource "azurerm_resource_group" "rg-git" {
   location = "central India" 
 }
 
-# resource "azurerm_resource_group" "rg-git2" {
-#   name     = "rg-git2"
-#   location = "central India"
-# }
+resource "azurerm_resource_group" "rg-git2" {
+  name     = "rg-git2"
+  location = "central India"
+}
